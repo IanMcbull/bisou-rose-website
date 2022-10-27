f7e703b39233ac52abc24198fc57e185e91200bb
IanMcbull <ianmugenya@gmail.com>
2022-10-27 21:03:50 +0300

    more ui updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import Contact from "./components/Contact"
 import Header from "./components/Header"
 import Services from "./components/Services"
 import 'mapbox-gl/dist/mapbox-gl.css';
+import Gallery from "./components/Gallery";
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Services/>
+      <Gallery/>
       <Contact/>
     </div>
   )
---
+++ src/components/Gallery.tsx
@@ -0,0 +1,33 @@
+import CardOne from "../assets/images/seats-image.jpg"
+import CardTwo from "../assets/images/nail-bar-wide-2-image.jpg"
+import CardThree from "../assets/images/mirrors-image.jpg"
+import CardFour from "../assets/images/macbook-table-image.jpg"
+import CardFive from "../assets/images/nail-polish-image.jpg"
+function Gallery() {
+  return (
+    <section className="my-44">
+        <h2 className="text-center text-6xl font-Cinzel font-semibold mb-24">Gallery</h2>
+        <div className="min-h-[50vh] container mx-auto grid grid-cols-2 grid-rows-2 gap-10">
+        <div className="row-span-2">
+            <img src={CardOne} alt="" className="w-full h-full object-cover rounded-md"/>
+        </div>
+        <div className="row-span-2 flex basis-1 flex-wrap gap-10">
+            <div className="max-w-[300px]">
+                <img src={CardTwo} alt="" className="w-full h-full object-cover rounded-md"/>
+            </div>
+            <div className="max-w-[300px]">
+                <img src={CardThree} alt="" className="w-full h-full object-cover rounded-md"/>
+            </div>
+            <div className="max-w-[300px]">
+                <img src={CardFour} alt="" className="w-full h-full object-cover rounded-md"/>
+            </div>
+            <div className="max-w-[300px]">
+                <img src={CardFive} alt="" className="w-full h-full object-cover rounded-md"/>
+            </div>
+        </div>
+    </div>
+    </section>
+  )
+}
+
+export default Gallery
