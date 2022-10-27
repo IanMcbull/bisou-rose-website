495ce9647cc01bd72106ddfbc49dd0fac5dce33d
IanMcbull <ianmugenya@gmail.com>
2022-10-27 07:42:55 +0300

    Updates to the landing page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import background from "../assets/images/corousel-1.jpg";
+import Logo from "../assets/images/logo.png";
 const styles = {
   backgroundImage: `linear-gradient(rgba(4,9,30,0.7),rgba(4,9,30,0.6)),url(${background})`,
   backgroundSize: 'cover',
@@ -7,9 +8,14 @@ const styles = {
 }
 function Header() {
   return (
-    <header className="min-h-[100vh] w-full kenburns-bottom" style={styles}>
-        <nav>
-
+    <header className="min-h-[100vh] kenburns-top-right" style={styles}>
+        <nav className="container mx-auto flex justify-between items-center py-10">
+          <img src={Logo} alt="Logo image for Bisou Rose" className="w-44"/>
+          <div className="nav-links flex">
+            <a href="#" className="text-white text-lg font-semibold">Home</a>
+            <a href="#" className="text-white text-lg font-semibold ml-28">Home</a>
+            <a href="#" className="text-white text-lg font-semibold ml-28">Home</a>
+          </div>
         </nav>
     </header>
   )
