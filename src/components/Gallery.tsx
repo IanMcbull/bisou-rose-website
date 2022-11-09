0fd0656b808e63bad29d0b39400fe89c67424f04
IanMcbull <ianmugenya@gmail.com>
2022-11-09 14:04:52 +0300

    gallery section updates

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -6,8 +6,20 @@ import CardFive from "../assets/images/nail-polish-image.jpg"
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 function Gallery() {
   return (
-      <section className="my-44 p-4" id="gallery">
-          <h2 className="text-center text-6xl font-Cinzel font-semibold mb-24">Gallery</h2>
+      <section className="min-h-[100vh]">
+         <div className="container mx-auto gallery font-Cinzel">
+            <div className="content flex flex-col justify-evenly">
+                 <h2 className="text-7xl">Bisou <span className="text-[rgb(129,77,42)]">Gallery</span></h2>
+                 <p className="text-justify font-semibold text-sm">We are excited to welcome you to our beauty salon, bringing a touch of London to Nairobi! 🥂</p> 
+            </div>
+            <div className="image-one rounded-md"></div>
+            <div className="image-two rounded-md"></div>
+            <div className="image-three rounded-md"></div>
+            <div className="image-four rounded-md"></div>
+            <div className="image-five rounded-md"></div>
+         </div>
+
+          {/* <h2 className="text-center text-6xl font-Cinzel font-semibold mb-24">Gallery</h2>
           <div className="min-h-[50vh] container mx-auto grid grid-cols-1 grid-rows-1 lg:grid-cols-2 lg:grid-rows-2 gap-10">
           <div className="row-span-1 md:row-span-2">
               <LazyLoadImage src={CardOne} alt="" className="w-full h-full object-cover rounded-md" effect="blur"/>
@@ -26,7 +38,7 @@ function Gallery() {
                   <LazyLoadImage src={CardFive} alt="" className="w-full h-full object-cover rounded-md" effect="blur"/>
               </div>
           </div>
-      </div>
+      </div> */}
       </section>
   );
 }
